4ad149fbc03b0cefe4c8dedbc97dad88cf425a00
Lu!s <Lu!s@DESKTOP-9TVIG3M>
2016-06-08 03:29:24 -0600

    Merge origin/master
    Conflicts:
    	src/estadistica01/Estadistica01.java

--- src/PackExtra/NewClass.java
+++ src/PackExtra/NewClass.java
@@ -11,7 +11,9 @@ package PackExtra;
  */
 public class NewClass {
     public static void main(String[] args) {
-        Error error = new Error(25.0,5.0,40.0,95.0);
-        System.out.println(error.zeta(Double.NaN));
+        //Error error = new Error(media,desviacion,tamanio,nc);
+        Error error = new Error(3100.0,150.0,100.0,95.0);
+        //System.out.println(error.zeta(Double.NaN));
+        System.out.println("El intervalo va desde: "+error.IConfianzaInferior()+" Hasta "+error.IConfianzaSuperior());
     }
 }
